d4ab2a38a5022d92c3ad6f91175fe07d3eb614be
Vania <Van.carvajal@duocuc.cl>
2026-05-15 17:55:17 -0400

    first commit

---
+++ bff-ventas/src/main/java/fitProject/ventas/bff_ventas/BffVentasApplication.java
@@ -0,0 +1,13 @@
+package fitProject.ventas.bff_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffVentasApplication.class, args);
+	}
+
+}
---
+++ bff-ventas/src/test/java/fitProject/ventas/bff_ventas/BffVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package fitProject.ventas.bff_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
